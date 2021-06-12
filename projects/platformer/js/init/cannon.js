3fdc1e743480c6e80f50fc13a2c66e765ea0439a
sneakzz13 <ethanjlandry05@gmail.com>
2021-06-12 03:34:17 +0000

    add cannons to game

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -28,6 +28,8 @@
         
         // example: 
         createCannon("top", 450);
+        createCannon("right", 400, 5000);
+        createCannon("top", 100);
         
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
